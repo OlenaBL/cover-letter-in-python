e5fac4a2f76b2e7b73bb76ae54b8dc82ec9d1a40
OlenaBL <35238049+OlenaBL@users.noreply.github.com>
2019-01-16 10:19:42 -0700

    Add files via upload

---
+++ cover letter in python.py
@@ -0,0 +1,29 @@
+coder = 'Olena'
+my_skills_1 = 'HTML5' + 'CSS' + 'JavaScript'
+job_skills_1 = 'HTML5' + 'CSS' + 'JavaScript'
+dev_job = 'front-end developer'
+
+def job_1():
+  if (my_skills_1 == job_skills_1):
+    return coder + ' is great for ' + dev_job +' position!'
+  else:
+    return 'Lets look at other possibilities'
+print(job_1())
+
+my_skills_2 = 'Python' + 'Linguistics'
+job_skills_2 = 'Python' + 'Linguistics'
+ling_job = 'computational linguist'
+
+def job_2():
+  if (my_skills_2 == job_skills_2):
+    return coder + ' is great for ' + ling_job +' position!'
+  else:
+    return 'Lets look at other possibilities'
+print(job_2())
+
+other_skills = 'international sales and marketing' + 'business development' + 'teaching foreign languages' + 'digital content development' 
+soft_skills = 'strong desire and ability to learn' + 'ability to receive constructive criticism and work under pressure'
+job_3 = 'So many possible positions for ' + coder + '!'
+print(job_3)
+
+
